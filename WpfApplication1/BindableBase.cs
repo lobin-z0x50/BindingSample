a30215ec5a46b58dca727b55d1bffe5069f917fd
Wataru Maeda <w.maeda@neogenia.co.jp>
2015-11-14 14:59:32 +0900

    initial commit

---
+++ WpfApplication1/BindableBase.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    /// <summary>
+    /// モデルを簡略化するための <see cref="INotifyPropertyChanged"/> の実装。
+    /// </summary>
+    public abstract class BindableBase : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// プロパティの変更を通知するためのマルチキャスト イベント。
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// プロパティが既に目的の値と一致しているかどうかを確認します。必要な場合のみ、
+        /// プロパティを設定し、リスナーに通知します。
+        /// </summary>
+        /// <typeparam name="T">プロパティの型。</typeparam>
+        /// <param name="storage">get アクセス操作子と set アクセス操作子両方を使用したプロパティへの参照。</param>
+        /// <param name="value">プロパティに必要な値。</param>
+        /// <param name="propertyName">リスナーに通知するために使用するプロパティの名前。
+        /// この値は省略可能で、
+        /// CallerMemberName をサポートするコンパイラから呼び出す場合に自動的に指定できます。</param>
+        /// <returns>値が変更された場合は true、既存の値が目的の値に一致した場合は
+        /// false です。</returns>
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] String propertyName = null)
+        {
+            if (object.Equals(storage, value)) return false;
+
+            storage = value;
+            this.RaisePropertyChanged(propertyName);
+            return true;
+        }
+
+        /// <summary>
+        /// プロパティ設定のラッパー
+        /// ラムダ式で設定先を指定できるバージョン
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="propertyExp"></param>
+        /// <param name="value"></param>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        protected bool SetProperty<T>(Expression<Func<T>> propertyExp, T value, [CallerMemberName] String propertyName = null)
+        {
+            var typeOfT = Expression.Parameter(propertyExp.Body.Type);
+
+            // ラムダ式にラップしてGetterとして呼び出す
+            var getter = propertyExp.Compile();
+            var oldVal = getter();
+            if (oldVal != null && oldVal.Equals(value) || oldVal == null && value == null) return false;
+
+            // 代入式を生成してラムダ式にラップし、Setterとして呼び出す
+            var assign = Expression.Lambda<Action<T>>(Expression.Assign(propertyExp.Body, typeOfT), typeOfT);
+            var setter = assign.Compile();
+            setter(value);
+            this.RaisePropertyChanged(propertyName);
+            return true;
+        }
+
+        /// <summary>
+        /// プロパティ値が変更されたことをリスナーに通知します。
+        /// </summary>
+        /// <param name="propertyName">リスナーに通知するために使用するプロパティの名前。
+        /// この値は省略可能で、
+        /// <see cref="CallerMemberNameAttribute"/> をサポートするコンパイラから呼び出す場合に自動的に指定できます。</param>
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var eventHandler = this.PropertyChanged;
+            if (eventHandler != null)
+            {
+                eventHandler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        /// <summary>
+        /// ラムダ式でメンバ名を指定できるバージョン（よりタイプセーフ）
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="propertyExpression"></param>
+        protected void RaisePropertyChanged<T>(Expression<Func<T>> propertyExpression)
+        {
+            if (propertyExpression.Body is MemberExpression)
+            {
+                RaisePropertyChanged(((MemberExpression)propertyExpression.Body).Member.Name);
+            }
+        }
+    }
+}
---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class Table1
+        {
+            public string _FirstName { get; set; }
+
+        }
+
+
+        public class ViewModel : Models.BindableBase
+        {
+            public Table1 _t1;
+
+            // 氏名
+            private string _text1;
+
+            public string Text1
+            {
+                get { return _text2 + "　" + _t1._FirstName; }
+                //set { _text1 = value; RaisePropertyChanged("Text1"); }
+            }
+
+            // 名前
+
+            public string FirstName
+            {
+                get { return _t1._FirstName; }
+                set { base.SetProperty(()=> _t1._FirstName, value); RaisePropertyChanged(nameof(Text1)); } 
+            }
+
+            // 名字
+            private string _text2;
+
+            public string FamilyName
+            {
+                get { return _text2; }
+                set { _text2 = value; RaisePropertyChanged(nameof(FamilyName)); RaisePropertyChanged(nameof(Text1)); }
+            }
+
+        }
+
+        public ViewModel vm;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            vm = new ViewModel();
+            vm._t1 = new Table1()
+            {
+                _FirstName = "前田"
+            };
+
+            this.DataContext = vm;
+        }
+
+        /// <summary>
+        /// ボタン押下時
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+          //  vm.Text1 = "WWWWW";
+            // textBox1.Text = "ZZZZZ";
+
+//            textBlock2.Text = "YYYY";
+        }
+    }
+}
